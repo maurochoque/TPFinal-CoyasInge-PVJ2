72fd36d61ce8b655a87d4cb6db57499a4e91bb8f
Barry <barrionuevoluiscarlos51@gmail.com>
2024-11-28 23:47:40 -0300

    ultima actualizacion
    un error menos y photon fuera

--- tp/Assets/Juego/Scripts/Enemies/MovimientoEnemigo.cs
+++ tp/Assets/Juego/Scripts/Enemies/MovimientoEnemigo.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MovimientoEnemigo : MonoBehaviour
 {
-    public LayerMask layer;
     public LayerMask suelo;
     public Transform controlador, ground; 
     private Rigidbody2D rigid;
@@ -15,19 +14,10 @@ public class MovimientoEnemigo : MonoBehaviour
     public int danioGolpe;
     public float distancia;
 
-    public bool isPool;
-
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        anim = FindObjectOfType<Animator>();
-    }
-
-    void Start()
-    {
-        /*punto1.parent = null;
-        punto2.parent = null;
-        StartCoroutine("Patrulla");*/
+        anim = FindObjectOfType<Animator>(); 
     }
 
     private void Update() {
@@ -39,93 +29,7 @@ public class MovimientoEnemigo : MonoBehaviour
         }
     }
 
-    /*IEnumerator Patrulla()
-    {
-        while (true)
-        {
-            anim.SetBool("walk", true);
-            // Verificar si el jugador está dentro del rango de visión
-            Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, 5f, layer);
-            if (playerCollider != null)
-            {
-                // Calcular la dirección hacia el jugador
-                Vector2 directionToPlayer = (playerCollider.transform.position - transform.position).normalized;
-                // Si el jugador está dentro del rango de ataque, detenerse y atacar
-                if (Vector2.Distance(transform.position, playerCollider.transform.position) <= rango_ataque)
-                {
-                    rigid.velocity = Vector2.zero;
-                    yield return new WaitForSeconds(1);
-                    anim.SetBool("attack", true);
-                    Golpear();
-                }
-                else // Si el jugador está dentro del rango de visión pero fuera del rango de ataque, perseguir al jugador
-                {
-                    rigid.velocity = directionToPlayer * speed_run;
-                    if(enemy.position.x > punto1.position.x || enemy.position.x < punto2.position.x)
-                    {
-                        anim.SetBool("walk", false);
-                        rigid.velocity = Vector2.zero;
-                    }
-                    if (directionToPlayer.x > 0) // Si el jugador está a la derecha, mirar hacia la derecha
-                        transform.rotation = Quaternion.Euler(0, 0, 0);
-                    else // Si el jugador está a la izquierda, mirar hacia la izquierda
-                        transform.rotation = Quaternion.Euler(0, 180, 0);
-                }
-            }
-            else // Si el jugador no está dentro del rango de visión, continuar patrullando
-            {
-                // Movimiento de patrulla
-                if (movingRight)
-                {
-                    rigid.velocity = new Vector2(speed_walk, rigid.velocity.y);
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                    if (enemy.position.x > punto1.position.x)
-                    {
-                        anim.SetBool("walk", false);
-                        rigid.velocity = Vector2.zero;
-                        yield return new WaitForSeconds(2f);
-                        movingRight = false;
-                    }
-                }
-                else
-                {
-                    rigid.velocity = new Vector2(-speed_walk, rigid.velocity.y);
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
-                    if (enemy.position.x < punto2.position.x)
-                    {
-                        anim.SetBool("walk", false);
-                        rigid.velocity = Vector2.zero;
-                        yield return new WaitForSeconds(2f);
-                        movingRight = true;
-                    }
-                }
-            }
-            yield return null;
-        }
-    }*/
-
-    private void OnTriggerEnter2D(Collider2D other){
-        //Collider2D[] objetos = Physics2D.OverlapCircleAll(controlador.position, rango_ataque);
-        //other = Physics2D.OverlapCircleAll(controlador.position, rango_ataque);
-        /*foreach(Collider2D colisionador in other)
-        {
-            //Daño dano = colisionador.transform.GetComponent<Daño>();
-            if(colisionador.CompareTag("Player"))
-            {        
-                //dano.TomarDano(danioGolpe);
-                player.CmdTakeDamage(10);
-                AudioManager.instance.Reproducir(15);
-                if(player.health == 0)
-                {
-                    AudioManager.instance.Reproducir(4);
-                    SceneManager.LoadScene("MenuDerrota");
-                }
-            }else if(colisionador.CompareTag("Escudo"))
-            {
-                AudioManager.instance.Reproducir(2);
-                print("Bloqueo");
-            }
-        }*/
+    /*private void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){        
             //dano.TomarDano(danioGolpe);
             //AudioManager.instance.Reproducir(15);
@@ -138,12 +42,12 @@ public class MovimientoEnemigo : MonoBehaviour
                 //AudioManager.instance.Reproducir(4);
                 SceneManager.LoadScene("MenuDerrota");
             }
-        }/*else if(other.CompareTag("Escudo"))
+        }else if(other.CompareTag("Escudo"))
         {
             AudioManager.instance.Reproducir(2);
             print("Bloqueo");
-        }*/
-    }
+        }
+    }*/
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
